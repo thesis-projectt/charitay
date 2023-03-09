73953be6a7f0b6f4a03c89759676541ad772ecbf
medalihadjsalah <mouhamedalim85@gmail.com>
2023-03-09 11:48:09 +0100

    test

--- clientt/App.js
+++ clientt/App.js
@@ -1,17 +1,9 @@
 import { StyleSheet, Text, View, SafeAreaView, TextInput } from "react-native";
-<<<<<<< HEAD
 import Map from "./Map1/Map";
 // import Card from "./Map1/Cart";
 // import SigninScreen from "./screens/SigninScreen/SigninScreen";
 // import SignupScreen from "./screens/SignupScreen/SignupScreen";
 
-=======
-// import Map from "./Map1/Map";
-
-// import SigninScreen from "./screens/SigninScreen/SigninScreen";
-// import SignupScreen from "./screens/SignupScreen/SignupScreen";
-
->>>>>>> 36da7d6f718d4c68fc572554718af663da685242
 // import ForgetPassword from "./screens/ForgetPassword/ForgetPassword";
 // import Navigation from "./screens/Navigation/Navigation";
 // import Onboarding from "./components/Onboarding"
@@ -20,21 +12,6 @@ import Map from "./Map1/Map";
 // import SigninScreen from "./screens/SigninScreen/SigninScreen";
 // import SignupScreen from "./screens/SignupScreen/SignupScreen";
 // import ForgetPassword from "./screens/ForgetPassword/ForgetPassword";
-<<<<<<< HEAD
-// import Navigation from "./screens/Navigation/Navigation";
-// import Onboarding from "./components/Onboarding"
-const App = () => {
-  return (
-              
-
-    <SafeAreaView style={styles.root}>
-      {/* <Card/> */}
-            <Map/>
-           {/* //   <Onboarding/> */}
-           {/* <SigninScreen/> */}
-            {/* <Navigation/> */}
-   </SafeAreaView>
-=======
 import Navigation from "./screens/Navigation/Navigation";
 // import Onboarding from "./components/Onboarding"
 import { NativeBaseProvider} from "native-base";
@@ -50,7 +27,6 @@ const App = () => {
            {/* <Navigation/> */}
             <Eventimage/>
    </SafeAreaView></NativeBaseProvider>
->>>>>>> 36da7d6f718d4c68fc572554718af663da685242
   );
 };
 const styles = StyleSheet.create({
